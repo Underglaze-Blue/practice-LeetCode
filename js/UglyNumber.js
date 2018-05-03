2fc4cfdfb810c00cb091c0e4cd2b9633a216ce7d
Underglaze-Blue <lepapillonangel@gmail.com>
2018-05-03 17:55:12 +0800

    添加两数之和，添加链接

--- js/UglyNumber.js
+++ js/UglyNumber.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/ugly-number/description/
 // Write a program to check whether a given number is an ugly number.
 
 // Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
